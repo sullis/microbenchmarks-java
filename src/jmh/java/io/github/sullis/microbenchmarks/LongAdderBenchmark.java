31b393b405e6e2f94d9bff826982ba33f60f68dc
Sean C. Sullivan <github@seansullivan.com>
2022-12-20 10:41:53 -0800

    AtomicLong, AtomicInteger

--- src/jmh/java/io/github/sullis/microbenchmarks/LongAdderBenchmark.java
+++ src/jmh/java/io/github/sullis/microbenchmarks/LongAdderBenchmark.java
@@ -2,6 +2,8 @@
 package io.github.sullis.microbenchmarks;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
 
 import org.openjdk.jmh.annotations.Benchmark;
@@ -15,10 +17,14 @@ import org.openjdk.jmh.annotations.State;
 @State(Scope.Thread)
 public class LongAdderBenchmark {
     private LongAdder longAdder;
+    private AtomicLong atomicLong;
+    private AtomicInteger atomicInteger;
 
     @Setup
     public void setUp() throws Exception {
         longAdder = new LongAdder();
+        atomicInteger = new AtomicInteger();
+        atomicLong = new AtomicLong();
     }
 
     @BenchmarkMode(Mode.Throughput)
@@ -29,5 +35,25 @@ public class LongAdderBenchmark {
         final long afterIncrement = longAdder.longValue();
         longAdder.decrement();
         final long afterDecrement = longAdder.longValue();
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @Benchmark
+    public void testAtomicLong() {
+        atomicLong.incrementAndGet();
+        final long afterIncrement = atomicLong.longValue();
+        atomicLong.decrementAndGet();
+        final long afterDecrement = atomicLong.longValue();
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @Benchmark
+    public void testAtomicInteger() {
+        atomicInteger.incrementAndGet();
+        final long afterIncrement = atomicInteger.longValue();
+        atomicInteger.decrementAndGet();
+        final long afterDecrement = atomicInteger.longValue();
     }
 }
