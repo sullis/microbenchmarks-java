99a47fc80e88a05d879a546915b58959368acedc
Sean C. Sullivan <github@seansullivan.com>
2022-12-23 13:45:28 -0800

    fix gradle syntax error

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,14 +11,14 @@ testing {
     }
 }
 
+java {                                      
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 allprojects {
   repositories {
     mavenCentral()
-  }
-
-  java {                                      
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
   }
 }
 
