a3c54b64a9da052ed6a978568792313b57234abf
Sean C. Sullivan <github@seansullivan.com>
2022-12-25 19:20:46 -0800

    add enum:  RandomSupplier

--- random/src/jmh/java/io/github/sullis/microbenchmarks/RandomBenchmark.java
+++ random/src/jmh/java/io/github/sullis/microbenchmarks/RandomBenchmark.java
@@ -10,13 +10,29 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 
 @State(Scope.Thread)
 public class RandomBenchmark {
-    private static final Supplier<Random> javaUtilRandomSupplier = new Supplier<Random>() {
+    @Param
+    private RandomSupplier randomSupplier;
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @Benchmark
+    public void measureRandom(final Blackhole bh) {
+        final Random r = this.randomSupplier.get();
+        bh.consume(r.nextInt());
+        bh.consume(r.nextLong());
+        bh.consume(r.nextFloat());
+        bh.consume(r.nextDouble());
+        bh.consume(r.nextBoolean());
+    }
+
+    private static final Supplier<java.util.Random> javaUtilRandomSupplier = new Supplier<Random>() {
         private static Random random = new java.util.Random();
         @Override
         public Random get() {
@@ -24,34 +40,26 @@ public class RandomBenchmark {
         }
     };
 
-    private static final Supplier<Random> threadLocalRandomSupplier = new Supplier<Random>() {
+    private static final Supplier<java.util.Random> threadLocalRandomSupplier = new Supplier<Random>() {
         @Override
         public Random get() {
             return ThreadLocalRandom.current();
         }
     };
 
-    @BenchmarkMode(Mode.Throughput)
-    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    @Benchmark
-    public void measureJavaUtilRandom(final Blackhole bh) {
-        exerciseRandom(bh, javaUtilRandomSupplier);
-    }
+    public enum RandomSupplier {
+        JAVA_UTIL_RANDOM(javaUtilRandomSupplier),
+        THREAD_LOCAL_RANDOM(threadLocalRandomSupplier);
 
-    @BenchmarkMode(Mode.Throughput)
-    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    @Benchmark
-    public void measureThreadLocalRandom(final Blackhole bh) {
-        exerciseRandom(bh, threadLocalRandomSupplier);
-    }
+        private final Supplier<java.util.Random> supplier;
 
-    private void exerciseRandom(final Blackhole bh, final Supplier<Random> randomSupplier) {
-        final Random r = randomSupplier.get();
-        bh.consume(r.nextInt());
-        bh.consume(r.nextLong());
-        bh.consume(r.nextFloat());
-        bh.consume(r.nextDouble());
-        bh.consume(r.nextBoolean());
+        RandomSupplier(Supplier<java.util.Random> randomSupplier) {
+            this.supplier = randomSupplier;
+        }
+
+        public java.util.Random get() {
+            return this.supplier.get();
+        }
     }
 
 }
