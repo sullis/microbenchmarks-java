8382fb3434f58895ce35fc8881c16d52ad2fa91f
Sean C. Sullivan <github@seansullivan.com>
2023-02-04 21:32:07 -0800

    zuul response 'setBody' benchmark

---
+++ zuul/build.gradle.kts
@@ -0,0 +1,5 @@
+
+dependencies {
+    jmh("com.netflix.zuul:zuul-core:2.3.0")
+    jmh("org.apache.commons:commons-lang3:3.12.0")
+}
---
+++ zuul/src/jmh/java/io/github/sullis/microbenchmarks/ResponseSetBodyBenchmark.java
@@ -0,0 +1,56 @@
+
+package io.github.sullis.microbenchmarks;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import com.netflix.zuul.context.SessionContext;
+import com.netflix.zuul.message.Headers;
+import com.netflix.zuul.message.http.HttpQueryParams;
+import com.netflix.zuul.message.http.HttpRequestMessage;
+import com.netflix.zuul.message.http.HttpRequestMessageImpl;
+import com.netflix.zuul.message.http.HttpResponseMessageImpl;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+import com.netflix.zuul.message.http.HttpResponseMessage;
+import org.apache.commons.lang3.RandomStringUtils;
+
+@State(Scope.Thread)
+public class ResponseSetBodyBenchmark {
+
+    private HttpResponseMessage responseMessage;
+    private String body;
+    private byte[] bodyByteArray;
+
+    @Setup
+    public void setup() {
+        body = RandomStringUtils.randomAlphabetic(2000);
+        bodyByteArray = body.getBytes(StandardCharsets.UTF_8);
+        SessionContext sessionContext = new SessionContext();
+        HttpRequestMessage request = new HttpRequestMessageImpl(sessionContext, "http", "GET", "/", new HttpQueryParams(), new Headers(), "127.0.0.1", "http", 8080, "servername");
+        responseMessage = new HttpResponseMessageImpl(sessionContext, request, 200);
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @Benchmark
+    public void setBodyByteArray(final Blackhole bh) {
+        responseMessage.setBody(bodyByteArray);
+        bh.consume(responseMessage);
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @Benchmark
+    public void setBodyString(final Blackhole bh) {
+        responseMessage.setBodyAsText(body);
+        bh.consume(responseMessage);
+    }
+}
