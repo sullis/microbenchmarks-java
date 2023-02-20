c6558d3846e4100c6f88153451ed9b22c0dbd2bf
Sean C. Sullivan <github@seansullivan.com>
2023-02-19 17:05:30 -0800

    add CompressionBenchmark

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,4 +60,9 @@ dependencies {
     jmh("com.netflix.netflix-commons:netflix-commons-util:0.3.0")
     jmh("com.fasterxml.uuid:java-uuid-generator:4.0.1")
     jmh("com.datastax.oss:java-driver-core:4.15.0")
+    jmh("com.aayushatharva.brotli4j:brotli4j:1.9.0")
+    jmh("com.aayushatharva.brotli4j:native-linux-x86_64:1.9.0")
+    jmh("com.aayushatharva.brotli4j:native-linux-aarch64:1.9.0")
+    jmh("com.aayushatharva.brotli4j:native-osx-x86_64:1.9.0")
+    jmh("com.aayushatharva.brotli4j:native-osx-aarch64:1.9.0")
 }
---
+++ src/jmh/java/io/github/sullis/microbenchmarks/CompressionBenchmark.java
@@ -0,0 +1,102 @@
+
+package io.github.sullis.microbenchmarks;
+
+import com.aayushatharva.brotli4j.Brotli4jLoader;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import java.util.zip.GZIPOutputStream;
+import com.aayushatharva.brotli4j.encoder.BrotliOutputStream;
+
+@State(Scope.Thread)
+public class CompressionBenchmark {
+
+    @Param
+    private CompressionType compressionType;
+    @Param(value = { "1000", "100000" })
+    private int size;
+    private String text;
+    private byte[] textBytes;
+    private ByteArrayOutputStream baos;
+
+    @Setup
+    public void setup() {
+        Brotli4jLoader.ensureAvailability();
+        text = RandomStringUtils.random(size, "ab");
+        textBytes = text.getBytes(StandardCharsets.UTF_8);
+        baos = new ByteArrayOutputStream(size);
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @Benchmark
+    public void compress(final Blackhole bh) throws Exception {
+        CompressionOps ops = this.compressionType.supplier.get();
+        ops.compress(textBytes, baos);
+        bh.consume(baos);
+    }
+
+    public enum CompressionType {
+
+        GZIP(new Supplier<CompressionOps>() {
+            @Override
+            public CompressionOps get() {
+                return new CompressionOps() {
+
+                    @Override
+                    public void compress(byte[] data, OutputStream out) throws IOException {
+                        GZIPOutputStream gzip = new GZIPOutputStream(out);
+                        gzip.write(data);
+                        gzip.flush();
+                        gzip.close();
+
+                    }
+                };
+            }
+        }),
+        BROTLI(new Supplier<CompressionOps>() {
+            @Override
+            public CompressionOps get() {
+                return new CompressionOps() {
+
+                    @Override
+                    public void compress(byte[] data, OutputStream out) throws IOException {
+                        BrotliOutputStream brotli = new BrotliOutputStream(out);
+                        brotli.write(data);
+                        brotli.flush();
+                        brotli.close();
+                    }
+                };
+            }
+        });
+
+        private final Supplier<CompressionOps> supplier;
+
+        CompressionType(Supplier<CompressionOps> supplier) {
+            this.supplier = supplier;
+        }
+
+        public Supplier<CompressionOps> getSupplier() {
+            return this.supplier;
+        };
+
+    }
+
+    public interface CompressionOps {
+        void compress(byte[] data, OutputStream out) throws IOException;
+    }
+}
