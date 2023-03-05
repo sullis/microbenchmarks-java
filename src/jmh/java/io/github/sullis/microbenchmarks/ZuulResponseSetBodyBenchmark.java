c3aeb390c97d69892611a7a674824f0e658a8773
Sean C. Sullivan <github@seansullivan.com>
2023-03-04 22:52:33 -0800

    ZuulResponseSetBodyBenchmark bodySize 1000

--- src/jmh/java/io/github/sullis/microbenchmarks/ZuulResponseSetBodyBenchmark.java
+++ src/jmh/java/io/github/sullis/microbenchmarks/ZuulResponseSetBodyBenchmark.java
@@ -28,7 +28,7 @@ public class ZuulResponseSetBodyBenchmark {
     private HttpResponseMessage responseMessage;
     private String body;
     private byte[] bodyByteArray;
-    @Param(value = { "0", "100", "1000", "10000", "100000"})
+    @Param(value = { "1000" })
     private int bodySize;
 
     @Setup
