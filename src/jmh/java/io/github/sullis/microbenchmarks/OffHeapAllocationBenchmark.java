cfa1b0ed8bcc6e6f6015fdfa5e595086c3499eb7
sullis <github@seansullivan.com>
2023-03-04 20:11:19 -0800

    offheap benchmark (#3)

---
+++ src/jmh/java/io/github/sullis/microbenchmarks/OffHeapAllocationBenchmark.java
@@ -0,0 +1,92 @@
+
+package io.github.sullis.microbenchmarks;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@Threads(value = Threads.MAX)
+public class OffHeapAllocationBenchmark {
+    @Param(value = "1024")
+    private int bufferSize;
+
+    @Param
+    private OffHeapAllocationType allocationType;
+    private OffHeapAllocationOps allocationOps;
+
+    @Setup
+    public void setup() throws Exception {
+        allocationOps = allocationType.newInstance();
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @Benchmark
+    public void measure(final Blackhole bh) {
+        final Object buffer = allocationOps.allocateBuffer(bufferSize);
+        allocationOps.releaseBuffer(buffer);
+        bh.consume(buffer);
+    }
+
+    public enum OffHeapAllocationType {
+        NETTY4(Netty4Allocation.class);
+        // NETTY5(Netty5Allocation.class);
+
+        private final Class<? extends OffHeapAllocationOps> clazz;
+
+        OffHeapAllocationType(Class<? extends OffHeapAllocationOps> clazz) {
+            this.clazz = clazz;
+        }
+
+        public OffHeapAllocationOps newInstance() throws Exception {
+            return clazz.newInstance();
+        }
+    }
+
+    interface OffHeapAllocationOps {
+        Object allocateBuffer(int size);
+        void releaseBuffer(Object obj);
+    }
+
+    class Netty4Allocation implements OffHeapAllocationOps {
+        private final io.netty.buffer.ByteBufAllocator alloc = io.netty.buffer.PooledByteBufAllocator.DEFAULT;
+
+        public Netty4Allocation() { }
+
+        @Override
+        public Object allocateBuffer(int size) {
+            return alloc.directBuffer(size);
+        }
+
+        @Override
+        public void releaseBuffer(Object obj) {
+            io.netty.util.ReferenceCountUtil.release(obj);
+        }
+    }
+
+    class Netty5Allocation implements OffHeapAllocationOps {
+        private final io.netty5.buffer.BufferAllocator alloc = io.netty5.buffer.DefaultBufferAllocators.offHeapAllocator();
+
+        public Netty5Allocation() { }
+
+        @Override
+        public Object allocateBuffer(int size) {
+            return alloc.allocate(size);
+        }
+
+        @Override
+        public void releaseBuffer(Object obj) {
+            io.netty5.util.ReferenceCountUtil.release(obj);
+        }
+    }
+}
