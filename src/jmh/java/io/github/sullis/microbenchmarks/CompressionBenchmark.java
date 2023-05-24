4b0813aad18324c21b69c9dab576d8c8bbfa3568
Sean C. Sullivan <github@seansullivan.com>
2023-05-24 16:56:50 -0700

    CompressionBenchmark: 100000.txt

--- src/jmh/java/io/github/sullis/microbenchmarks/CompressionBenchmark.java
+++ src/jmh/java/io/github/sullis/microbenchmarks/CompressionBenchmark.java
@@ -38,7 +38,7 @@ public class CompressionBenchmark {
     private static final ConcurrentHashMap<String, byte[]> DATA = new ConcurrentHashMap<>();
     @Param
     private CompressionType compressionType;
-    @Param(value = { "10000.txt" })
+    @Param(value = { "10000.txt", "100000.txt" })
     private String filename;
     private byte[] textBytes;
 
