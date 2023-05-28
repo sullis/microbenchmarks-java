8b140591a94b03bb219872b787c12adf19e1e913
Sean C. Sullivan <github@seansullivan.com>
2023-05-28 09:34:24 -0700

    jackson 2.14.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ val zuulOssVersion = "2.3.1-SNAPSHOT"
 
 dependencies {
     jmh("org.springframework:spring-web:6.0.9")
-    jmh("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    jmh("com.fasterxml.jackson.core:jackson-databind:2.14.3")
     jmh("com.google.guava:guava:31.1-jre")
     jmh("com.github.ben-manes.caffeine:caffeine:3.1.2")
     jmh("org.apache.commons:commons-lang3:3.12.0")
