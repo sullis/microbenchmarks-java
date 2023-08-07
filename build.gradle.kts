9040ea1bc21e40585c4fccc3700c61d73738bbc3
sullis <github@seansullivan.com>
2023-08-06 21:43:11 -0700

    spring-web 6.0.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ val netty5Version = "5.0.0.Alpha5"
 val zuulOssVersion = "2.3.1-SNAPSHOT"
 
 dependencies {
-    jmh("org.springframework:spring-web:6.0.9")
+    jmh("org.springframework:spring-web:6.0.11")
     jmh("com.fasterxml.jackson.core:jackson-databind:2.14.3")
     jmh("com.google.guava:guava:31.1-jre")
     jmh("com.github.ben-manes.caffeine:caffeine:3.1.7")
