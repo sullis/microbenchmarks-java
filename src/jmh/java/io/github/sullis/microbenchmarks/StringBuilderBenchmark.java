796821a3d82d65940ca72f8bdc2b75568b2acab8
Sean C. Sullivan <github@seansullivan.com>
2023-09-09 16:09:54 -0700

    cleanup imports

--- src/jmh/java/io/github/sullis/microbenchmarks/StringBuilderBenchmark.java
+++ src/jmh/java/io/github/sullis/microbenchmarks/StringBuilderBenchmark.java
@@ -1,23 +1,17 @@
 
 package io.github.sullis.microbenchmarks;
 
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
-import java.util.SplittableRandom;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.infra.Blackhole;
 
 @State(Scope.Thread)
