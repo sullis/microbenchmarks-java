6b609feeed093d5f7552203678bd845c31a7988f
sullis <github@seansullivan.com>
2024-02-08 04:45:21 -0800

    netty 4.1.106

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ jmh {
 }
 
 val brotli4jVersion = "1.12.0"
-val nettyVersion = "4.1.100.Final"
+val nettyVersion = "4.1.106.Final"
 val netty5Version = "5.0.0.Alpha5"
 val zuulOssVersion = "2.4.1-SNAPSHOT"
 
