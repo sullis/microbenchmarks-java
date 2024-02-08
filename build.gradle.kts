1ffd765720c8a1cbafa0ccae653cc425623e8fea
sullis <github@seansullivan.com>
2024-02-08 04:42:19 -0800

    log4j 2.22.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,7 +70,7 @@ dependencies {
     jmh("io.netty:netty-common:$nettyVersion")
     jmh("io.netty:netty5-buffer:$netty5Version")
     jmh("io.netty:netty5-common:$netty5Version")
-    jmh("org.apache.logging.log4j:log4j-core:2.20.0")
+    jmh("org.apache.logging.log4j:log4j-core:2.22.1")
     jmh("org.slf4j:slf4j-api:1.7.36")
     jmh("com.netflix.netflix-commons:netflix-commons-util:0.3.0")
     jmh("com.fasterxml.uuid:java-uuid-generator:4.0.1")
