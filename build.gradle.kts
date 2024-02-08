26c7e19c4a2954ed2e6a8bfe541534c1732679e1
sullis <github@seansullivan.com>
2024-02-08 04:46:15 -0800

    brotli4j 1.16.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ jmh {
     profilers.set(listOf("gc"))
 }
 
-val brotli4jVersion = "1.12.0"
+val brotli4jVersion = "1.16.0"
 val nettyVersion = "4.1.106.Final"
 val netty5Version = "5.0.0.Alpha5"
 val zuulOssVersion = "2.4.1-SNAPSHOT"
