f385c32042caab63fcecf347434e429c8853d0c6
sullis <github@seansullivan.com>
2024-04-05 08:02:39 -0700

    zstd-jni 1.5.6-2

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     jmh("com.netflix.netflix-commons:netflix-commons-util:0.3.0")
     jmh("com.fasterxml.uuid:java-uuid-generator:4.0.1")
     jmh("com.datastax.oss:java-driver-core:4.15.0")
-    jmh("com.github.luben:zstd-jni:1.5.6-1")
+    jmh("com.github.luben:zstd-jni:1.5.6-2")
     jmh("com.aayushatharva.brotli4j:brotli4j:$brotli4jVersion")
     jmh("com.aayushatharva.brotli4j:native-linux-x86_64:$brotli4jVersion")
     jmh("com.aayushatharva.brotli4j:native-linux-aarch64:$brotli4jVersion")
