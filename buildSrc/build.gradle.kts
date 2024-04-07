b0e5a5389cb9c5b6f67789085c8ac6c5b76b4ebf
sullis <github@seansullivan.com>
2024-04-07 15:03:52 -0700

    kotlin 1.9.23

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java`
-    kotlin("jvm") version "1.9.20-RC"
+    kotlin("jvm") version "1.9.23"
 }
 
 repositories {
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20-RC")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
 }
